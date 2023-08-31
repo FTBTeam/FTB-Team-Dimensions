0f58cc3b75e4a112879cec38eabdf1fea68f5899
Des Herriott <des.herriott@gmail.com>
2023-08-31 15:54:46 +0100

    fix: also allow WaterLoggingFixProcessor to work on ServerLevel
    Previously only worked for WorldGenRegion, which would not be the case
    if being run during a `/place jigsaw` command

--- src/main/java/dev/ftb/mods/ftbteamdimensions/dimensions/waterlogging/WaterLoggingFixProcessor.java
+++ src/main/java/dev/ftb/mods/ftbteamdimensions/dimensions/waterlogging/WaterLoggingFixProcessor.java
@@ -3,8 +3,10 @@ package dev.ftb.mods.ftbteamdimensions.dimensions.waterlogging;
 import com.mojang.serialization.Codec;
 import dev.ftb.mods.ftbteamdimensions.registry.ModWorldGen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LevelWriter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
@@ -13,6 +15,8 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProc
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class WaterLoggingFixProcessor extends StructureProcessor {
     public static final WaterLoggingFixProcessor INSTANCE = new WaterLoggingFixProcessor();
@@ -23,19 +27,29 @@ public class WaterLoggingFixProcessor extends StructureProcessor {
     public StructureTemplate.StructureBlockInfo process(LevelReader levelReader, BlockPos blockPos, BlockPos blockPos2, StructureTemplate.StructureBlockInfo original, StructureTemplate.StructureBlockInfo after, StructurePlaceSettings structurePlaceSettings, @Nullable StructureTemplate template) {
         // NOTE: process() method is a Forge extension. If we ever port this to Fabric, use processBlock() instead
 
-        if (after.state.isAir() || !(levelReader instanceof WorldGenRegion worldGenRegion)) {
-            return after;
-        }
-
-        // Is the block meant to be a fluid? No then lets make sure the world doesn't already have a fluid there
-        if (after.state.getFluidState().isEmpty()) {
-            // Is the block water? No, is the current block in the world water? Yes, Cool, remove it before placement
-            if (after.state.getBlock() != Blocks.WATER && levelReader.hasChunkAt(after.pos) && levelReader.getBlockState(after.pos).getFluidState().is(Fluids.WATER)) {
-                worldGenRegion.setBlock(after.pos, after.state, Block.UPDATE_ALL);
-            }
+        if (!after.state.isAir()) {
+            getWriter(levelReader).ifPresent(writer -> {
+                // Is the block meant to be a fluid? No then lets make sure the world doesn't already have a fluid there
+                if (after.state.getFluidState().isEmpty()) {
+                    // Is the block water? No, is the current block in the world water? Yes, Cool, remove it before placement
+                    if (after.state.getBlock() != Blocks.WATER && levelReader.hasChunkAt(after.pos) && levelReader.getBlockState(after.pos).getFluidState().is(Fluids.WATER)) {
+                        writer.setBlock(after.pos, after.state, Block.UPDATE_ALL);
+                    }
+                }
+            });
         }
 
         return after;
+    }
+
+    private Optional<LevelWriter> getWriter(LevelReader reader) {
+        if (reader instanceof WorldGenRegion wg) {
+            return Optional.of(wg);
+        } else if (reader instanceof ServerLevel sl) {
+            return Optional.of(sl);
+        } else {
+            return Optional.empty();
+        }
     }
 
     @Override
